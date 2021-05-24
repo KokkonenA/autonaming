4c8c4d58fa573180d44508de3787bd60eda7165b
Antti Kokkonen <akn@saunalahti.fi>
2021-05-24 16:13:24 +0300

    fileName refactored to fileDate. Returns clip property 'Date Created'

--- autonaming.py
+++ autonaming.py
@@ -23,13 +23,19 @@ for clipIdx in clips:
 	elif 'Audio' in type:
 		audioFiles.append(clip)
 
-# add take numbers to metadata
+# sort files by date of creation
+def fileDate(file):
+	return file.GetClipProperty()['Date Created']
 
+videoFiles.sort(key = fileDate)
+audioFiles.sort(key = fileDate)
+
+# add take numbers to metadata
 scene = '0'
 take = 1
 
 for file in videoFiles:
-	newScene = file.GetMetadata('Scene')
+	newScene = file.GetMetadata()['Scene']
 	
 	if (newScene == scene):
 		take += 1
@@ -39,22 +45,19 @@ for file in videoFiles:
 
 	file.SetMetadata('Take', str(take))
 
-
+# returns true if no 'MOS' found in comments (audio was recorded to a separate file at the same time)
 def hasAudio(videoFile):
 	comments = videoFile.GetMetadata('Comments')
 	return (len(comments) == 0 or 'MOS' not in comments['Comments'])
 
+# filter only the takes for wich also audio was recorded
 videoFilesFiltered = filter(hasAudio, videoFiles)
 
-def fileName(file):
-	return file.GetClipProperty()['File Name']
-
-videoFilesFiltered.sort(key = fileName)
-audioFiles.sort(key = fileName)
-
-for i in range(0, len(videoFilesFiltered) - 1):
+for i in range(0, len(videoFilesFiltered)):
 	metadata = videoFilesFiltered[i].GetMetadata()
 	audioFile = audioFiles[i]
 
 	audioFile.SetMetadata('Scene', metadata['Scene'] )
 	audioFile.SetMetadata('Take', metadata['Take'])
+
+	print('Done!')
